a030bd98a1da2915e231812b62faec896d6c2fa9
noog1e <logan_white2@outlook.com>
2026-01-30 17:26:29 +0800

    Tested PlayerManager

--- tests/test_PlayerManager.cpp
+++ tests/test_PlayerManager.cpp
@@ -1,6 +1,7 @@
 #include "PlayerManager.hpp"
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
+#include <string>
 
 TEST_CASE("Default init check", "[player][init]"){
 
@@ -12,6 +13,54 @@ TEST_CASE("Default init check", "[player][init]"){
 
         REQUIRE(p.name == "UNASSIGNED");
         REQUIRE(p.type == EntityType::UNASSIGNED);
-        REQUIRE(p.marker == PlayerMarker::NONE);
+        REQUIRE(p.marker == BoardMarker::NONE);
+    }
+}
+
+TEST_CASE("Player Update Tests", "[player][update]"){
+
+    PlayerManager pm;
+
+    SECTION("Names"){
+
+        NameUpdate nu;
+
+        SECTION("Duplicates"){
+            
+            std::string n1 = "SAME";
+            std::string n2 = "SAME";
+
+            nu = pm.updateNames(n1, n2);
+
+            REQUIRE(nu == NameUpdate::DUPLICATES);
+        }
+
+        SECTION("Different"){
+            
+            std::string n1 = "SAME";
+            std::string n2 = "DIFFERENT";
+
+            nu = pm.updateNames(n1, n2);
+
+            REQUIRE(nu == NameUpdate::VALID);
+        }
+        
+    }
+
+    SECTION("Type"){
+
+        pm.updateType(EntityType::HUMAN, PlayerOne);
+        REQUIRE(pm.getPlayer(PlayerOne).type == EntityType::HUMAN);
+        
+        pm.updateType(EntityType::AI, PlayerTwo);
+        REQUIRE(pm.getPlayer(PlayerTwo).type == EntityType::AI);
+    }
+
+    SECTION("Marker"){
+        
+        pm.updateMarker(BoardMarker::CROSS, PlayerOne);
+
+        REQUIRE(pm.getPlayer(PlayerOne).marker == BoardMarker::CROSS);
+        REQUIRE(pm.getPlayer(PlayerTwo).marker == BoardMarker::NOUGHT);
     }
 }
